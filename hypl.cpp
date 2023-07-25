718ff707527df49956784c97896822bd4fe8ef56
Dragon267 <112898086+Dragon267@users.noreply.github.com>
2023-07-25 16:13:41 +0200

    Multiple Command Line Arguments

--- hypl.cpp
+++ hypl.cpp
@@ -91,11 +91,8 @@ int main(int argc, char* argv[]) {
 
   if (argc == 1) {
     repl();
-  } else if (argc == 2) {
-    run_file(argv[1]);
   } else {
-    fprintf(stderr, "[ERROR] Usage: hypl [path]\n");
-    exit(64);
+    run_file(argv[1]);
   }
 
   free_hvm();
